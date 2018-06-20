d689d4647fb098c403cc44d1769c57abeba3b13b
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 17:04:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,6 +41,15 @@ end
 
 def get_occupation(data, hometown)
   # code here
+  output = nil
+  data.each do |seas, person_array|
+    person_array.each do |person|
+      if person["hometown"] == hometown
+        output = person["occupation"]
+      end
+    end
+  end
+  output
 end
 
 def get_average_age_for_season(data, season)
